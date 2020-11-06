208b7792084b1341816659e420af6726781d48a6
sokolova-an <Anastasiia_Sokolova2@epam.com>
2020-11-06 12:33:39 +0300

    nuxt SSR

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam vero eum ex
+      ipsum recusandae delectus beatae ut temporibus tempore? Aut quibusdam,
+      aliquam perferendis corrupti corporis itaque autem. Eos, delectus
+      reprehenderit!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam vero eum ex
+      ipsum recusandae delectus beatae ut temporibus tempore?
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ["auth"]
+};
+</script>
